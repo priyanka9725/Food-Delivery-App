e56f8dd1583890e0263cf217e543ff467074d0ad
priyanka9725 <priyanka97xx@gmail.com>
2023-12-14 02:30:19 +0530

    Making Frontend

--- desi-cravings/src/App.jsx
+++ desi-cravings/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function App() {
   return (
--- desi-cravings/src/screens/Home.jsx
+++ desi-cravings/src/screens/Home.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <Navbar />
-        <div class="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Important Text</p>
-            <div className="container w-100">
-              <select className="m-2 h-100 w-100 bg-success">{Array.from(Array(6), (e,x)=>{
-                return(
-                  <option key={x+1} value={x+1}> x+1 </option>
-                )
-              })}</select>
-            </div>
-          </div>
-        </div>
+      <Navbar />
+      <Carousel />
+      <div className="m-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
+
       <Footer />
     </div>
   );
