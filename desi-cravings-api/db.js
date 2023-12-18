6670a1a453d380e1e2bf7efee1059bf11a4ce4ca
priyanka9725 <priyanka97xx@gmail.com>
2023-12-18 20:42:19 +0530

    trying to connect to database

--- desi-cravings-api/db.js
+++ desi-cravings-api/db.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb+srv://desicravings:mysecondapp@cluster0.ovlvi4x.mongodb.net/";
+  "mongodb+srv://desicravings:mysecondapp@cluster0.ovlvi4x.mongodb.net/?retryWrites=true&w=majority";
 const mongoDB = async () => {
-  await mongoose.connect(mongoURI, (err) => {
-    if (!err) console.log("DB Connected");
-    const fetchData = async () => {
-      await mongoose.connection.db.collection("food_items");
-      fetchData.find({}).toArray(function (err, data) {
-        if (err) console.log(err);
-        else console.log(data);
-      });
-    };
-  });
+  await mongoose.connect(mongoURI).then()(console.log("DB Connected"));
 };
--- desi-cravings-api/server.js
+++ desi-cravings-api/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const mongoDB = require("./db");
 
+
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
